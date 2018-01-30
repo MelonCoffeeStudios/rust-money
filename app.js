471eaca36577a8a4cece3be361ca61bda5bc4aea
Aaron <aaronjgriffin96@gmail.com>
2018-01-30 17:14:58 +0000

    hih

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var csvWriter = require('csv-write-stream');
 var fs = require("fs")
 var io = require('socket.io-client');
+var path = require("path");
 var socket = io.connect('https://rustmoney.com:2096', {query:  "token=b14541fc642cfaa8fdfbb285a2f23a3256bf4fcdb06cf43f998c310c4e0e29e93a50f20e24e85d0a185bb907245b43e78df" +
     "be6fc9512509d05ae34f9b13a98799d8756d8eb2261f13926462b27e6f9c6481d7ee41e9b3a6119932e8dbd7285aae35336db638719625b24b1" +
     "ecaa6475dad6858391aa7a3655ba9245742a5e9c74a0e204d9735fdd3f6b8b5570293181b9",reconnect: true});
@@ -14,7 +15,9 @@ const express = require('express')
 const app = express()
 var port = 80;
 
-
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'out.csv'));
+})
 
 
 
