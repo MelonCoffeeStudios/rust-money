7ca2cae4841b7044ea49234f9631a81e59739f11
Aaron <aaronjgriffin96@gmail.com>
2018-02-01 13:35:45 +0000

    Ayyy

--- app.js
+++ app.js
@@ -30,10 +30,11 @@ app.get("/", function (req, res) {
 io.on('connection', function(socket){
     console.log('a user connected');
     getCSV(function (data) {
+        socket.emit("totalDraws",data.length);
         data.forEach(function (draw, index, collection) {
             setTimeout(function () {
                 socket.emit("addOneItem", {data:draw});
-            }, index * 1000)
+            }, index * 50)
 
         });
 
